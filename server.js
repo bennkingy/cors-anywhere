ce514ed9fea8fa12e3e36689c61c23d40d91bb32
Benn <benn.king@intelogy.co.uk>
2019-12-16 09:50:44 +0000

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://abfstg.sharepoint.com/sites/ABFI','https://localhost:4321/temp/workbench.html','https://abfoods.sharepoint.com/sites/abi-pulse'],
+  originWhitelist: ['https://abfstg.sharepoint.com/sites/ABFI','https://localhost:4321/temp/workbench.html','https://abfoods.sharepoint.com/sites/abi-pulse', 'https://localhost:4321/'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
