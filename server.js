c3a0e56365527e91687275a5b6d41e9114eaf95f
Benn <benn.king@intelogy.co.uk>
2019-12-19 14:10:10 +0000

    Whitelist does not seem to be working...

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://bpkintelogydev.sharepoint.com/sites/ABFI','https://abfstg.sharepoint.com/sites/ABFI','https://localhost:4321/temp/workbench.html','https://abfoods.sharepoint.com/sites/abi-pulse', 'https://localhost:4321', 'https://abfoods.sharepoint.com/sites/abi-pulse', 'https://abfoods.sharepoint.com/sites/abi-hub', 'https://abfoods.sharepoint.com/sites/abe-hub', 'https://abfoods.sharepoint.com/sites/abt-hub', 'https://abfoods.sharepoint.com/sites/ohl-hub', 'https://abfoods.sharepoint.com/sites/pgp-hub', 'https://abfoods.sharepoint.com/sites/spi-hub'],
+  originWhitelist: originWhitelist, 
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
@@ -42,3 +42,5 @@ cors_proxy.createServer({
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
+
+//['https://bpkintelogydev.sharepoint.com/sites/ABFI','https://abfstg.sharepoint.com/sites/ABFI','https://localhost:4321/temp/workbench.html','https://abfoods.sharepoint.com/sites/abi-pulse', 'https://localhost:4321', 'https://abfoods.sharepoint.com/sites/abi-pulse', 'https://abfoods.sharepoint.com/sites/abi-hub', 'https://abfoods.sharepoint.com/sites/abe-hub', 'https://abfoods.sharepoint.com/sites/abt-hub', 'https://abfoods.sharepoint.com/sites/ohl-hub', 'https://abfoods.sharepoint.com/sites/pgp-hub', 'https://abfoods.sharepoint.com/sites/spi-hub'],
